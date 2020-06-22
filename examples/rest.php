561d0b027948f12dbd6397aac1d46600402a9227
Ramon Fincken <github@managedwphosting.nl>
2020-06-22 11:11:04 +0200

    [TASK] REST examples

---
+++ examples/rest.php
@@ -0,0 +1,22 @@
+### Is logged in?
+@since 4.6.0
+
+<script>
+<?php mwp_noprov_bool_isloggedin_script(); ?>
+</script>
+
+
+once the AJAX call has been fired you will have the JS json (bool) variable managedwphosting_hasitemsincart_bool at your disposal
+so .. NOT before, it will return undefined
+
+
+### Has any items in WooCommerce cart?
+@since 4.6.0
+
+<script>
+<?php mwp_noprov_bool_hasitemsincart_script(); ?>
+</script>
+
+
+once the AJAX call has been fired you will have the JS json (bool) variable managedwphosting_hasitemsincart_bool at your disposal
+so .. NOT before, it will return undefined
