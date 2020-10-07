3bb26a80a52ee139c4b0235755df461d38b8d714
Ramon Fincken <github@managedwphosting.nl>
2020-10-07 12:46:33 +0200

    [TASK] add AJAX

---
+++ examples/ajax.php
@@ -0,0 +1,5 @@
+### Disable all plugins on AJAX call
+@since 4.7.7
+
+Use GET or POST param "mwp_no_plugins" to disable loading of ALL plugins during an AJAX call.
+Usefull when needing a faster loading time on your theme AJAX calls
